348a84d0c133bc66936844072baf00e86fe47840
Eliot Szwajkowski <eliotpszw@gmail.com>
2018-03-03 14:57:33 -0500

    Review I

--- Review1/index.js
+++ Review1/index.js
@@ -12,4 +12,50 @@ console.log(chalk[returnRandomColor()]('------------------------------'));
 
 const c = (...args) => console.log(...args);
 
-// Visual Studio Code
+// Let vs Var
+
+// Let is actually quite different.
+
+// Block Scoping
+
+// We think of scope as being only created by functions
+// let actually creates scope.
+
+// Given a number, I want to make a function that returns an array of functions that will log which number they are.
+
+// createLoggers(5);
+// [func0, func1, ...];
+// func0() // -> 0
+
+// function createLoggers(ceiling) {
+//   var loggerArray = [];
+
+//   for (let i = 0; i < ceiling; ++i) {
+//     loggerArray.push(function() {
+//       console.log(i);
+//     })
+//   }
+
+//   return loggerArray;
+// }
+
+
+// var loggerArray = createLoggers(5);
+// console.log(loggerArray);
+
+// loggerArray.forEach(func => func());
+
+// A functional for loop
+
+
+const functionalForLoop = (floor, ceiling, callback) => {
+  const results = [];
+  
+  while (results.length < ceiling - floor) {
+    results.push(callback(results.length))
+  }
+}
+
+const someCollection = ['hi', 'bye', 'goodbye'];
+
+functionalForLoop(0, someCollection.length, (idx) => console.log(someCollection[idx]));
